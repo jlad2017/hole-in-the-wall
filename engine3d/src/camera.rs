b03b52a14e0a3c34916500d1b1f0a54ca38c2d8f
Adam Guo <agsmguo@gmail.com>
2021-04-29 11:57:25 +0800

    Add OrbitCamera

--- engine3d/src/camera.rs
+++ engine3d/src/camera.rs
@@ -15,3 +15,55 @@ impl Camera {
         (view, proj)
     }
 }
+
+#[derive(Clone, Debug)]
+pub struct OrbitCamera {
+    pub pitch: f32,
+    pub yaw: f32,
+    pub distance: f32,
+    player_pos: Pos3,
+    player_rot: Quat,
+}
+
+impl Camera for OrbitCamera {
+    fn new() -> Self {
+        Self {
+            pitch: 0.0,
+            yaw: 0.0,
+            distance: 5.0,
+            player_pos: Pos3::new(0.0, 0.0, 0.0),
+            player_rot: Quat::new(1.0, 0.0, 0.0, 0.0),
+        }
+    }
+    fn update(&mut self, events: &engine3d::events::Events, player: &Player) {
+        let (dx, dy) = events.mouse_delta();
+        self.pitch += dy / 100.0;
+        self.pitch = self.pitch.clamp(-PI / 4.0, PI / 4.0);
+        self.yaw += dx / 100.0;
+        self.yaw = self.yaw.clamp(-PI / 4.0, PI / 4.0);
+        if events.key_pressed(KeyCode::Up) {
+            self.distance -= 0.5;
+        }
+        if events.key_pressed(KeyCode::Down) {
+            self.distance += 0.5;
+        }
+        self.player_pos = player.body.c;
+        self.player_rot = player.rot;
+        // TODO: when player moves, slightly move yaw towards zero
+    }
+    fn update_camera(&self, c: &mut engine3d::camera::Camera) {
+        // The camera should point at the player
+        c.target = self.player_pos;
+        // And rotated around the player's position and offset backwards
+        c.eye = self.player_pos
+            + (self.player_rot
+                * Quat::from(cgmath::Euler::new(
+                    cgmath::Rad(self.pitch),
+                    cgmath::Rad(self.yaw),
+                    cgmath::Rad(0.0),
+                ))
+                * Vec3::new(0.0, 0.0, -self.distance));
+        // To be fancy, we'd want to make the camera's eye to be an object in the world and whose rotation is locked to point towards the player, and whose distance from the player is locked, and so on---so we'd have player OR camera movements apply accelerations to the camera which could be "beaten" by collision.
+    }
+}
+
